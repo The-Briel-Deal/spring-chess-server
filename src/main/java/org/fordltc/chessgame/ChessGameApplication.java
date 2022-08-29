544169e54b398fa918eedb5defcb8d53fd6bdba7
Gabriel <gabe@fordltc.net>
2022-08-29 18:35:00 -0400

    created hello world endpoint

--- src/main/java/org/fordltc/chessgame/ChessGameApplication.java
+++ src/main/java/org/fordltc/chessgame/ChessGameApplication.java
@@ -2,12 +2,18 @@ package org.fordltc.chessgame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ChessGameApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChessGameApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChessGameApplication.class, args);
-	}
-
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
 }
