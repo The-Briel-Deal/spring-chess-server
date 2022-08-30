0e73e17f0fd4ebcce763297322df54aaa8cc5e23
Gabriel <gabe@fordltc.net>
2022-08-29 21:04:41 -0400

    added method to change board

--- src/main/java/org/fordltc/chessgame/ChessGameApplication.java
+++ src/main/java/org/fordltc/chessgame/ChessGameApplication.java
@@ -2,8 +2,11 @@ package org.fordltc.chessgame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
@@ -15,9 +18,29 @@ public class ChessGameApplication {
         SpringApplication.run(ChessGameApplication.class, args);
     }
 
+    @Configuration
+    public class WebConfiguration implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**").allowedMethods("*");
+        }
+    }
+
     @GetMapping("/getBoard")
-    public String hello() {
+    public String getBoard() {
         ChessBoard chessBoard = ChessBoard.getInstance();
+        return Arrays.deepToString(chessBoard.board);
+    }
+
+    @GetMapping("/changeBoard")
+    public String changeBoard() {
+        ChessBoard chessBoard = ChessBoard.getInstance();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                chessBoard.board[i][j]++;
+            }
+        }
         return Arrays.deepToString(chessBoard.board);
     }
 }
